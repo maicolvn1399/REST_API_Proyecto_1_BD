068b4af89bae3251e678f2a169614ab262f1b214
Michael Valverde <maicolvn1399@gmail.com>
2023-05-07 17:17:48 -0600

    [Added] comments to ProductController

--- REST_API_GymTEC/Controllers/ProductController.cs
+++ REST_API_GymTEC/Controllers/ProductController.cs
@@ -5,10 +5,17 @@ using System.Data;
 
 namespace REST_API_GymTEC.Controllers
 {
+    /// <summary>
+    /// Product Controller 
+    /// </summary>
     [ApiController]
     [Route("api")]
     public class ProductController : ControllerBase
     {
+        /// <summary>
+        /// HTTP GET method to get all the products stored in the database 
+        /// </summary>
+        /// <returns>json with all the products listed </returns>
         [HttpGet("get_all_products")]
 
         public async Task<ActionResult<JSON_Object>> GetAllProducts()
@@ -47,6 +54,11 @@ namespace REST_API_GymTEC.Controllers
             }
 
         }
+        /// <summary>
+        /// HTTP POST method to get a specific product 
+        /// </summary>
+        /// <param name="barras"> refers to the identifier to get the information for a specific product </param>
+        /// <returns>json with the information of the requested product </returns>
         [HttpPost("get_product")]
 
         public async Task<ActionResult<JSON_Object>> GetProduct(Product_Identifier barras)
@@ -82,6 +94,11 @@ namespace REST_API_GymTEC.Controllers
             }
 
         }
+        /// <summary>
+        /// HTTP POST method to add a new product to the database 
+        /// </summary>
+        /// <param name="new_product"> refers to the new product that will be stored in the database </param>
+        /// <returns> returns a json with the status confirming if the query was succesfull or not </returns>
         [HttpPost("add_product")]
 
         public async Task<ActionResult<JSON_Object>> AddProduct(Product new_product)
@@ -100,6 +117,11 @@ namespace REST_API_GymTEC.Controllers
             }
 
         }
+        /// <summary>
+        /// HTTP PUT method to update a product 
+        /// </summary>
+        /// <param name="updated_product"> refers to an updated product</param>
+        /// <returns> returns a json with the status confirming if the query was succesfull or not </returns>
         [HttpPut("update_product")]
         public async Task<ActionResult<JSON_Object>> UpdateProduct(Product updated_product)
         {
@@ -116,6 +138,11 @@ namespace REST_API_GymTEC.Controllers
             }
 
         }
+        /// <summary>
+        /// HTTP DELETE method to delete a product from the database 
+        /// </summary>
+        /// <param name="barras"> identifier to delete a specific product from the database </param>
+        /// <returns> returns a json with the status confirming if the query was succesfull or not </returns>
         [HttpDelete("delete_product")]
         public async Task<ActionResult<JSON_Object>> DeleteProduct(Product_Identifier barras)
         {
@@ -132,6 +159,11 @@ namespace REST_API_GymTEC.Controllers
             }
 
         }
+        /// <summary>
+        /// HTTP POST method to associate a product to a branch 
+        /// </summary>
+        /// <param name="obj"> refers to the product that will be associated with a specific branch </param>
+        /// <returns> returns a json with the status confirming if the query was succesfull or not </returns>
         [HttpPost("associate_product")]
         public async Task<ActionResult<JSON_Object>> AssignProduct(Associate_product obj)
         {
